088fb5d4b645de8ec0eb20fbd9be8144392f5df7
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-10-18 23:30:14 +0300

    [MsSQL] Bugfixing on migrations - ForeignKeys attributes added.

--- FurnitureFactory.Models/Product.cs
+++ FurnitureFactory.Models/Product.cs
@@ -1,14 +1,26 @@
 ﻿namespace FurnitureFactory.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class Product
     {
         public int Id { get; set; }
 
         public int? FurnitureTypeId { get; set; }
 
+        [ForeignKey("FurnitureTypeId")]
+        public virtual FurnitureType FurnitureType { get; set; }
+
         public int? RoomId { get; set; }
 
+        [ForeignKey("RoomId")]
+        public virtual Room Room { get; set; }
+
         public int? SeriesId { get; set; }
+
+        [ForeignKey("SeriesId")]
+        public virtual Series Series { get; set; }
 
         public decimal ProductionExpense { get; set; }
 
@@ -17,12 +29,8 @@
 
         public double Weight { get; set; }
 
+        [Required]
+        [MaxLength(10)]
         public string CatalogNumber { get; set; }
-
-        public virtual FurnitureType FurnitureType { get; set; }
-
-        public virtual Room Room { get; set; }
-
-        public virtual Series Series { get; set; }
     }
 }
