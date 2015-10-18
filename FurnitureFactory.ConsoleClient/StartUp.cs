09091088af6568640244557230f73bfade8856b5
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-10-18 15:14:06 +0300

    ConsoleClient added

---
+++ FurnitureFactory.ConsoleClient/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace FurnitureFactory.ConsoleClient
+{
+    using Data.MongoDb;
+    using FurnitureFactory.Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+             var db = new FurnitureFactoryDbContext();
+            var DatabaseName = "furnitures";
+
+            db.Database.Delete();
+            db.Database.Create();
+            db.SaveChanges();
+
+            var io = new UserInterfaceIO();
+            var mongodata = new MongoDbData(DatabaseName, io);
+            mongodata.Import(db);
+        }
+    }
+}
---
+++ FurnitureFactory.ConsoleClient/UserInterfaceIO.cs
@@ -0,0 +1,18 @@
+﻿namespace FurnitureFactory.ConsoleClient
+{
+    using System;
+    using Logic;
+
+    public class UserInterfaceIO : UserInterfaceHandlerIO
+    {
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public void SetOutput(object obj)
+        {
+           Console.WriteLine(obj);
+        }
+    }
+}
