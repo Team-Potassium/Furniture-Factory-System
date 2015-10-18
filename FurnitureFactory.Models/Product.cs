d0941d626bbde16212513043f20eb109cfb09b83
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-10-18 15:03:50 +0300

    Product Model for MsSQL

---
+++ FurnitureFactory.Models/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace FurnitureFactory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int FurnitureTypeId { get; set; }
+
+        public int RoomId { get; set; }
+
+        public int SeriesId { get; set; }
+
+        public decimal ProductionExpense { get; set; }
+
+       // TODO: Change to appropriate type if have time
+        public int ProductionTime { get; set; }
+
+        public double Weight { get; set; }
+
+        public string CatalogNumber { get; set; }
+
+        public virtual FurnitureType FurnitureType { get; set; }
+
+        public virtual Room Room { get; set; }
+
+        public virtual Series Series { get; set; }
+    }
+}
