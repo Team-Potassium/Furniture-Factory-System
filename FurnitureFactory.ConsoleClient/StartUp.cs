f3387a45138a67586730ebbb9780b2ed5ca8319e
todorm85 <todorbm85@gmail.com>
2015-10-19 05:18:59 +0300

    Add SalesReportsLoad
    Intoduced FilesSystemUtils project to store classes for reading and writing certian file types to the file system.  Implemented Composite pattern via IArchiveLoader that has many IFileLoaders, so simultaneous loading of various file types with single archive read can be made. You can even read archives inside archives and so on.
    
    In Logic assembly added DataLoaders namespace to store various loaders of data to a destination DbContext. Added SalesReportsLoader to write to FurnitureDataDbContext.Clients. So far only the new clients that are read from an archive are added to Clients DbSet and saved to the database.

--- FurnitureFactory.ConsoleClient/StartUp.cs
+++ FurnitureFactory.ConsoleClient/StartUp.cs
@@ -2,7 +2,9 @@
 {
     using System.Linq;
     using Data.MongoDb;
+    using FileSystemUtils.FileLoaders;
     using FurnitureFactory.Data;
+    using FurnitureFactory.Logic.DataLoaders;
 
     public class StartUp
     {
@@ -10,15 +12,33 @@
 
         public static void Main()
         {
-            var context = new FurnitureFactoryDbContext();
-            context.Database.Delete();
-            context.Database.Create();
-            context.SaveChanges();
+            //var context = new FurnitureFactoryDbContext();
+            //context.Database.Delete();
+            //context.Database.Create();
+            //context.SaveChanges();
 
-            ConsoleUserInterfaceIO io = new ConsoleUserInterfaceIO();
-            var mongodata = new MongoDbData(DatabaseName, io);
-            mongodata.Import(context);
-            io.SetOutput(context.Products.Count());
+            //ConsoleUserInterfaceIO io = new ConsoleUserInterfaceIO();
+            //var mongodata = new MongoDbData(DatabaseName, io);
+            //mongodata.Import(context);
+            //io.SetOutput(context.Products.Count());
+
+            // Load excel from zip - Task1
+            LoadSalesReports();
+        }
+
+        private static void LoadSalesReports()
+        {
+            string sourceArchiveFilePath = @"..\..\..\Sales-Reports.zip";
+
+            var salesReportLoader = new SalesReportsDataLoader();
+
+            var excelFileLoader = new ExcelFileLoader();
+            excelFileLoader.AddDataLoader(salesReportLoader);
+
+            var zipArchiveLoader = new ZipArchiveLoader();
+            zipArchiveLoader.AddFileLoader(excelFileLoader);
+
+            zipArchiveLoader.Load(sourceArchiveFilePath);
         }
     }
 }
