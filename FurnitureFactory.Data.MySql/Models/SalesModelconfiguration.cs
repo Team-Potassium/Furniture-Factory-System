305e4077f60f1b7a4e1f23836c2eb568ea206c84
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-10-19 18:10:58 +0300

    Model mapping with Open Access

---
+++ FurnitureFactory.Data.MySql/Models/SalesModelconfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureFactory.Data.MySql.Models
+{
+    using Telerik.OpenAccess.Metadata.Fluent;
+
+    public partial class SalesModelConfiguration : FluentMetadataSource
+    {
+        protected override IList<MappingConfiguration> PrepareMapping()
+        {
+            List<MappingConfiguration> configurations =
+                new List<MappingConfiguration>();
+
+            var salesMapping = new MappingConfiguration<SalesTotalCostReport>();
+            salesMapping.MapType(report => new
+            {
+                Id = report.ID,
+                Name = report.Name,
+                EmailAddress = report.EmailAddress,
+                DateCreated = report.DateCreated
+            }).ToTable("Customer");
+            salesMapping.HasProperty(c => c.ID).IsIdentity();
+
+            configurations.Add(salesMapping);
+
+            return configurations;
+        }
+    }
+}
