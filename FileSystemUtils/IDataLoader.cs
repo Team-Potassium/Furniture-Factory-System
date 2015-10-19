db357027cafa8f5ee5583e9592e0c7a3480253a2
todorm85 <todorbm85@gmail.com>
2015-10-19 05:59:06 +0300

    Fixed conflicts and errors

---
+++ FileSystemUtils/IDataLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemUtils
+{
+    /// <summary>
+    /// Interface describing classes that load data to DbContext.
+    /// </summary>
+    public interface IDataLoader
+    {
+        void LoadData(IList<Object> data);
+    }
+}
