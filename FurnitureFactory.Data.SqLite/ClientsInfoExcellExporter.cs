0d6c0a573df85b6d333325a0d7c3adc73dea9c97
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-10-20 05:23:51 +0300

    Excell exporting from sqLite

--- FurnitureFactory.Data.SqLite/ClientsInfoExcellExporter.cs
+++ FurnitureFactory.Data.SqLite/ClientsInfoExcellExporter.cs
@@ -1,10 +1,15 @@
 ﻿namespace FurnitureFactory.Data.SqLite
 {
-    using System.ComponentModel.DataAnnotations;
-    using Data.SqLite;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.IO;
+    using Bytescout.Spreadsheet;
+    using Bytescout.Spreadsheet.Constants;
+
 
     public class ClientsInfoSqLiteExporter
     {
+        private string documentPath = "../../../CompaniesInfo.xls";
         private SqLiteData sqliteData;
 
         public ClientsInfoSqLiteExporter()
@@ -12,10 +17,64 @@
             this.sqliteData = new SqLiteData();
         }
 
-        public void Export()
+        public ClientsInfoSqLiteExporter Export()
         {
             var clientsInfo = this.sqliteData.GetAll();
 
+            Spreadsheet document = new Spreadsheet();
+
+            // add new worksheet
+            Worksheet sheet = document.Workbook.Worksheets.Add("Client contacts");
+
+            // headers to indicate purpose of the column
+            sheet.Cell("A1").Value = "Company Name";
+            sheet.Cell("B1").Value = "Email";
+            sheet.Cell("C1").Value = "Address";
+            sheet.Cell("D1").Value = "Iban";
+
+            Color headerColor = Color.FromArgb(80, 80, 80);
+            sheet.Rows[0].Height = 35;
+
+            for (int i = 0; i < 4; i++)
+            {
+                sheet.Cell(0, i).FillPattern = PatternStyle.Solid;
+                sheet.Cell(0, i).FillPatternForeColor = headerColor;
+                sheet.Cell(0, i).FontColor = Color.White;
+                sheet.Cell(0, i).Font = new Font("Arial", 14, FontStyle.Bold);
+                sheet.Cell(0,i).AlignmentHorizontal = AlignmentHorizontal.Centered;
+                sheet.Cell(0,i).AlignmentVertical = AlignmentVertical.Centered;
+                sheet.Columns[i].Width = 250;
+            }
+           
+            // delete output file if exists already
+            if (File.Exists(documentPath))
+            {
+                File.Delete(documentPath);
+            }
+
+            var row = 1;
+            foreach (var entity in clientsInfo)
+            {
+                sheet.Cell(row, 0).Value = entity.Name;
+                sheet.Cell(row, 1).Value = entity.Email;
+                sheet.Cell(row, 2).Value = entity.Address;
+                sheet.Cell(row, 3).Value = entity.Iban;
+                row++;
+            }
+
+
+            document.SaveAs(documentPath);
+
+            // Close Spreadsheet
+            document.Close();
+            return this;
+        }
+
+
+        public void Open()
+        {
+            // open generated XLS document in default program
+            Process.Start("../../../CompaniesInfo.xls");
         }
     }
 }
--- FurnitureFactory.Data.SqLite/StartUp.cs
+++ FurnitureFactory.Data.SqLite/StartUp.cs
@@ -1,10 +1,13 @@
 ﻿namespace FurnitureFactory.Data.SqLite
 {
+    using System.Diagnostics;
+
     public class StartUp
     {
         public static void Main()
         {
-            
+            new ClientsInfoSqLiteExporter().Export();
+
         }
     }
 }
